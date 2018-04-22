4470b74397a8ccf079e10ea3af948aeecccbdb9a
Mauricio Moccelin <mauriciomoccellin@hotmail.com>
2018-04-22 14:49:37 -0300

    Analisador léxico
    Geração de lexemas+tokens,

---
+++ LexicalAnalyzer/LexicalAnalyzer/ILexer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace LexicalAnalyzer
+{
+    public interface ILexer
+    {
+        void AddDefinition(TokenDefinition tokenDefinition);
+        IEnumerable<Token> Tokenize(string source);
+    }
+}
---
+++ LexicalAnalyzer/LexicalAnalyzer/TokenPosition.cs
@@ -0,0 +1,16 @@
+﻿namespace LexicalAnalyzer
+{
+    public class TokenPosition
+    {
+        public TokenPosition(int index, int line, int column)
+        {
+            Index = index;
+            Line = line;
+            Column = column;
+        }
+
+        public int Column { get; private set; }
+        public int Index { get; private set; }
+        public int Line { get; private set; }
+    }
+}
